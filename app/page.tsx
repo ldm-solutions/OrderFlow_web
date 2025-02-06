2859aed883d15c816167ea7c44841f6a962f8235
ldm-solutions <ldmcoutonunes@gmail.com>
2025-02-06 08:26:35 -0300

    Atualiza metadados do app e configurações do projeto para "OrderFlow"

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,9 @@ export default async function Home() {
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <div>
-          <GradientBadge title="Melhorias na versão 1.1.1" />
+          <GradientBadge
+            title={`Melhorias na versão ${process.env.NEXT_PUBLIC_APP_VERSION}`}
+          />
         </div>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
       </main>
